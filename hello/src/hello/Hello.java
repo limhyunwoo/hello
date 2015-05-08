e36a9e90659a0476a6ac3707f3af872195c24dda
취업반 PM < PM@itbank06-PC>
2015-05-08 21:35:29 +0900

    구구단이라고 제목을 주었음..

--- hello/src/hello/Hello.java
+++ hello/src/hello/Hello.java
@@ -2,6 +2,7 @@ package hello;
 
 class Hello {
 	public static void main(String[] args) {
+		System.out.println("±¸±¸´Ü");
 		for (int j = 1; j < 10; j++) {
 			for (int i = 2; i < 6; i++) {
 				System.out.print(i + "x" + j + "=" + (i * j) + "\t");
